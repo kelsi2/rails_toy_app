f57b70c1af62fa77e970dd9b0445e9a7dc367ed7
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-08-22 13:41:49 -0700

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  
+  # Go to users controller and run the index action
+  root 'users#index'
 end
